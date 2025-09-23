c33173c0ebe964dae5e4c0888d3742a82675e22d
KhumaerotuZahra <khumaerotuzahra18@students.unnes.ac.id>
2025-09-23 09:49:58 +0700

    benerinmedical

--- database/migrations/2025_09_22_143443_create_medical_records_table.php
+++ database/migrations/2025_09_22_143443_create_medical_records_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
         Schema::create('medical_records', function (Blueprint $table) {
             $table->id('id_record'); 
 
-            $table->foreignId('id_appointment')->constrained(table: 'appointments', column: 'id_appointment')->onDelete('cascade');
-            $table->foreignId('id_prescription')->constrained(table: 'prescriptions', column: 'id_prescription')->onDelete('cascade'); //cek
-
+            $table->foreignId('id_appointment')->constrained(table: 'appointments', column:'id_appointment')->onDelete('cascade');
+            $table->foreignId('id_prescription')->constrained(table: 'prescriptions', column: 'id_prescription')->onDelete('cascade'); 
             $table->string('diagnosis');
             $table->string('treatment');
             $table->text('notes')->nullable();
