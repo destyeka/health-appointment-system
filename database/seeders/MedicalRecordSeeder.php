543d4dae1c6069bd36e20c3d9beb4d9d6abf53c4
KhumaerotuZahra <khumaerotuzahra18@students.unnes.ac.id>
2025-09-23 19:47:34 +0700

    Ubah seeder medrec

--- database/seeders/MedicalRecordSeeder.php
+++ database/seeders/MedicalRecordSeeder.php
@@ -3,34 +3,31 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\MedicalRecord; // panggil modelnya
 
 class MedicalRecordSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        DB::table('medical_records')->insert([
-            [
-                'id_appointment' => 001, 
-                'diagnosis' => 'Demam dan flu',
-                'treatment' => 'Istirahat, minum obat flu',
-                'notes' => 'Kontrol ulang 3 hari lagi',
-            ],
-            [
-                'id_appointment' => 002,
-                'diagnosis' => 'Sakit gigi',
-                'treatment' => 'Cabut gigi + antibiotik',
-                'notes' => 'Pasien alergi paracetamol',
-            ],
-            [
-                'id_appointment' => 003,
-                'diagnosis' => 'Hipertensi ringan',
-                'treatment' => 'Diet rendah garam + obat hipertensi',
-                'notes' => 'Cek tekanan darah tiap minggu',
-            ],
+        MedicalRecord::create([
+            'id_appointment' => 1,
+            'diagnosis' => 'Demam dan flu',
+            'treatment' => 'Istirahat, minum obat flu',
+            'notes' => 'Kontrol ulang 3 hari lagi',
+        ]);
+
+        MedicalRecord::create([
+            'id_appointment' => 2,
+            'diagnosis' => 'Sakit gigi',
+            'treatment' => 'Cabut gigi + antibiotik',
+            'notes' => 'Pasien alergi paracetamol',
+        ]);
+
+        MedicalRecord::create([
+            'id_appointment' => 3,
+            'diagnosis' => 'Hipertensi ringan',
+            'treatment' => 'Diet rendah garam + obat hipertensi',
+            'notes' => 'Cek tekanan darah tiap minggu',
         ]);
     }
 }
