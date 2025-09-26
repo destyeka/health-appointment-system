d6c0975448c181a035463f261abf37a642cdcca6
KhumaerotuZahra <khumaerotuzahra18@students.unnes.ac.id>
2025-09-26 19:14:12 +0700

    Buat Role

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('roles')->insert([
+            [
+                'role_name' => 'Admin',
+                'description' => 'Mengelola sistem, user, dan konfigurasi aplikasi',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'role_name' => 'Doctor',
+                'description' => 'Melakukan konsultasi dan pelayanan medis',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'role_name' => 'Patient',
+                'description' => 'Mengakses layanan kesehatan dan melakukan appointment',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
