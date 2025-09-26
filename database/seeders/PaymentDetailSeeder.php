aeefaa17f86653728f5823fd7c1268a119bed0d2
destyeka <destyekasyawfitri@students.unnes.ac.id>
2025-09-26 19:56:45 +0700

    update seeder

--- database/seeders/PaymentDetailSeeder.php
+++ database/seeders/PaymentDetailSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\PaymentDetail;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Seeder;
 
 class PaymentDetailSeeder extends Seeder
